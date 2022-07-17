f0e8cbd80f4f5ecb614977c938a77a2c3dedfd35
Ziyaad <rhyman.ziyaad@gmail.com>
2022-07-17 12:19:36 +0400

    [+][UPD] Updated styles

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -4,19 +4,29 @@
       class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-2xl"
     >
       <img
-        class="w-full h-96 md:h-[350px] object-scale-down md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+        class="w-full h-96 md:h-[350px] object-scale-down md:w-48 rounded-t-lg md:p-3 md:rounded-none md:rounded-l-lg"
         :src="product.image"
         alt="Product-Image"
       />
-      <router-link :to="{ name: 'product', params: { id: product.id } }">
-        <div class="p-6 flex flex-col justify-start">
+      <router-link
+        :to="{ name: 'product', params: { id: product.id } }"
+        class="flex"
+      >
+        <div class="p-6 flex flex-col justify-center">
           <h5 class="text-gray-900 text-xl font-medium mb-2">
             {{ product.title }}
           </h5>
           <p class="text-gray-700 text-base mb-4 p-description">
             {{ product.description }}
           </p>
-          <p class="text-gray-600 text-xs">${{ product.price }}</p>
+          <p class="text-gray-600 text-xl font-bold mb-2 text-center">
+            ${{ product.price }}
+          </p>
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+          >
+            Add to Cart
+          </button>
         </div>
       </router-link>
     </div>
