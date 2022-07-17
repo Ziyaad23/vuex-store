d633d2e439226c9147d7e332ddcf79146546c876
Ziyaad <rhyman.ziyaad@gmail.com>
2022-07-17 11:34:42 +0400

    [+][UPD] Updated cart layout

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -5,7 +5,7 @@
         @click="show = !show"
         class="flex items-center p-2 text-white rounded-md font-bold"
       >
-        <span class="mr-4">Dropdown Menu</span>
+        <span class="mr-4">Cart (0)</span>
         <svg
           class="w-5 h-5 text-white"
           xmlns="http://www.w3.org/2000/svg"
@@ -21,25 +21,43 @@
       </button>
       <div
         v-show="show"
-        class="absolute right-0 py-2 mt-2 bg-white rounded-md shadow-xl w-44"
+        class="absolute right-0 py-2 mt-2 bg-white rounded-md shadow-xl w-60"
       >
         <router-link
           to="/"
           class="block px-4 py-2 text-sm text-black hover:bg-blue-500 hover:text-white"
         >
-          Dropdown List 1
+          <div class="flex justify-between">
+            <div>
+              <p>Product Title</p>
+              <p>1 x $23</p>
+            </div>
+            <div><a href="#">Remove</a></div>
+          </div>
         </router-link>
         <router-link
           to="/"
           class="block px-4 py-2 text-sm text-black hover:bg-blue-500 hover:text-white"
         >
-          Dropdown List 2
+          <div class="flex justify-between">
+            <div>
+              <p>Product Title</p>
+              <p>1 x $23</p>
+            </div>
+            <div><a href="#">Remove</a></div>
+          </div>
         </router-link>
         <router-link
           to="/"
           class="block px-4 py-2 text-sm text-black hover:bg-blue-500 hover:text-white"
         >
-          Dropdown List 3
+          <div class="flex justify-between">
+            <div>
+              <p>Product Title</p>
+              <p>1 x $23</p>
+            </div>
+            <div><a href="#">Remove</a></div>
+          </div>
         </router-link>
       </div>
     </div>
